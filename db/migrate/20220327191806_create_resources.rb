13126db5e9426e06e11b42a831cdf57f13411751
Anna Poltieva <anna.poltieva@kennasecurity.com>
2022-03-27 21:45:34 +0200

    Deal with CORS

--- db/migrate/20220327191806_create_resources.rb
+++ db/migrate/20220327191806_create_resources.rb
@@ -1,7 +1,10 @@
 class CreateResources < ActiveRecord::Migration[7.0]
   def change
     create_table :resources do |t|
-
+      t.string :name, null: false
+      t.text :description
+      t.string :url
+      t.integer :type, null: false
       t.timestamps
     end
   end
