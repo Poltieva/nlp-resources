d2f55dfcee400cb4b963661f6f26b13e50965d83
Anna Poltieva <poltyeva.anna@gmail.com>
2022-07-08 15:54:22 +0200

    Modify .gitignore and add author migration

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_03_175139) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_08_132632) do
   create_table "resources", force: :cascade do |t|
     t.string "name", null: false
     t.text "description"
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_03_175139) do
     t.integer "medium", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "author"
   end
 
 end
