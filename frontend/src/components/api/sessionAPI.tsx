b9c82dd78c2bc4030f1b9edaeb0c21f654b0ec5b
Anna Poltieva <poltyeva.anna@gmail.com>
2022-09-09 10:09:46 +0200

    WIP

---
+++ frontend/src/components/api/sessionAPI.tsx
@@ -0,0 +1,138 @@
+import axios from "./axios";
+
+const LOGIN_URL = "/oauth/token";
+const SIGNUP_URL = `${process.env.REACT_APP_API_URL}/users`;
+const UPDATE_PROFILE_URL = "/users";
+const LOGOUT_URL = "/oauth/revoke";
+const CURRENT_USER_URL = "/users/me";
+
+// TODO figure out why CLIENT_ID is undefined
+const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
+const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET;
+
+export async function createUserWithEmailAndPassword(
+    email: string,
+    password: string
+) {
+    const data = {
+        email: email,
+        password: password,
+        client_id: CLIENT_ID,
+    };
+
+    return axios
+        .post(SIGNUP_URL, data)
+        .then((response: any) => {
+            console.log('==========')
+            console.log(data)
+            return response.data;
+        })
+        .catch((error: any) => {
+            console.log('==========')
+            console.log(data)
+            return error.response.data;
+        });
+}
+
+export async function loginWithEmailAndPassword(
+    email: string,
+    password: string
+) {
+    const data = {
+        grant_type: "password",
+        email: email,
+        password: password,
+        client_id: CLIENT_ID,
+        client_secret: CLIENT_SECRET,
+    };
+
+    return axios
+        .post(LOGIN_URL, data)
+        .then((response: any) => {
+            return response.data;
+        })
+        .catch((error: any) => {
+            return error.response.data;
+        });
+}
+
+export async function updateUserProfile(
+    currentPassword: string,
+    token: string | undefined,
+    email?: string,
+    password?: string
+) {
+    const data = {
+        current_password: currentPassword,
+        email: email,
+        password: password,
+        client_id: CLIENT_ID,
+        client_secret: CLIENT_SECRET,
+    };
+    const config = {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    };
+
+    return axios
+        .patch(UPDATE_PROFILE_URL, data, config)
+        .then((response: any) => {
+            return response.data;
+        })
+        .catch((error: any) => {
+            return error.response.data;
+        });
+}
+
+export async function logoutUserWithToken(token: string) {
+    const data = {
+        token: token,
+        client_id: CLIENT_ID,
+        client_secret: CLIENT_SECRET,
+    };
+
+    return axios
+        .post(LOGOUT_URL, data)
+        .then((response: any) => {
+            return response.data;
+        })
+        .catch((error: any) => {
+            return error.response.data;
+        });
+}
+
+export async function requestAccessTokenWithRefreshToken(refreshToken: string) {
+    const data = {
+        grant_type: "refresh_token",
+        refresh_token: refreshToken,
+        client_id: CLIENT_ID,
+        client_secret: CLIENT_SECRET,
+    };
+
+    return axios
+        .post(LOGIN_URL, data)
+        .then((response: any) => {
+            return response.data;
+        })
+        .catch((error: any) => {
+            return error.response.data;
+        });
+}
+
+export async function getCurrentUser(accessToken: string) {
+    const config = {
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+        },
+    };
+
+    return axios
+        .get(CURRENT_USER_URL, config)
+        .then((response: any) => {
+            return response.data;
+        })
+        .catch((error: any) => {
+            return error.response.data;
+        });
+}
---
+++ frontend/src/components/sessions/PersistLogin.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { Outlet } from 'react-router-dom';
+import {RootState, store} from '../../store';
+import { refreshAccessToken } from './sessionSlice';
+
+type AppDispatch = typeof store.dispatch
+
+const useAppDispatch = () => useDispatch<AppDispatch>()
+function PersistLogin() {
+    const loading = useSelector((state: RootState) => state.session.loading);
+    const accessToken = useSelector((state : RootState) => state.session.accessToken);
+    const refreshToken = useSelector((state : RootState) => state.session.refreshToken);
+    const dispatch = useAppDispatch();
+
+    useEffect(() => {
+        function verifyRefreshToken() {
+            try {
+                dispatch(refreshAccessToken(refreshToken));
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        if (!accessToken) {
+            verifyRefreshToken();
+        }
+    }, [accessToken, refreshToken]);
+
+    return (
+        <>
+            {loading ? <p>Loading...</p> : <Outlet />}
+        </>
+    )
+}
+
+export default PersistLogin
