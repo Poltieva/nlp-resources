9577adc4ada19e404a831ddf1da0352467a2c1c5
Anna Poltieva <poltyeva.anna@gmail.com>
2022-10-03 14:51:17 +0200

    Add rescue from RecordNotFound

--- backend/app/controllers/api/v1/resources_controller.rb
+++ backend/app/controllers/api/v1/resources_controller.rb
@@ -2,6 +2,7 @@
 
 class Api::V1::ResourcesController < ApplicationController
   skip_before_action :doorkeeper_authorize! #, only: %i[index]
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
 
   def index
     render json: Resource.all, except: [:created_at, :updated_at]
@@ -16,4 +17,9 @@ class Api::V1::ResourcesController < ApplicationController
   def update; end
 
   def destroy; end
+
+  private
+  def not_found(exception)
+    render json: {errors: exception.full_message}, status: :not_found
+  end
 end
