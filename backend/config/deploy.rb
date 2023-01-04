ffb4170cf10ffce44e9472e2be7401d9f68150b1
Anna Poltieva <poltyeva.anna@gmail.com>
2023-01-04 15:45:27 +0100

    test

--- backend/config/deploy.rb
+++ backend/config/deploy.rb
@@ -11,7 +11,7 @@ set :puma_workers,    0
 set :branch,          :main
 
 # Don't change these unless you know what you're doing
-set :pty,             true
+set :pty,             false
 set :use_sudo,        false
 set :stage,           :production
 set :deploy_via,      :remote_cache
