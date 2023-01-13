e8aab341c797b84babaf3c14926a3650865ab376
Anna Poltieva <poltyeva.anna@gmail.com>
2023-01-13 14:25:00 +0100

    Test HashRouter

--- docs/src/App.js
+++ docs/src/App.js
@@ -1,23 +1,27 @@
 import AppRoutes from './components/AppRoutes';
-import { history } from './helpers/history';
+// import { history } from './helpers/history';
 import {Component} from "react";
-import {clearMessage} from "./actions/message";
-import {BrowserRouter} from "react-router-dom";
+// import {clearMessage} from "./actions/message";
+// import {BrowserRouter} from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-
-        history.listen((location) => {
-            props.dispatch(clearMessage()); // clear message when changing location
-        });
-    }
+    // constructor(props) {
+    //     super(props);
+    //
+    //     history.listen((location) => {
+    //         props.dispatch(clearMessage()); // clear message when changing location
+    //     });
+    // }
 
     render() {
         return (
-            <BrowserRouter history={history}>
+            // <BrowserRouter history={history}>
+            <Router>
                 <AppRoutes />
-            </BrowserRouter>
+            </Router>
+
+            // </BrowserRouter>
         )
     }
 }
