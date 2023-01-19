c37dbe8d958ca0a7a12a495ef6c3247079c91e79
Anna Poltieva <poltyeva.anna@gmail.com>
2023-01-19 10:57:03 +0100

    test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,67 +1,14 @@
-// // import AppRoutes from './components/AppRoutes';
-// // import { history } from './helpers/history';
-// import {Component} from "react";
-// // import {clearMessage} from "./actions/message";
-// // import {BrowserRouter} from "react-router-dom";
-// import {HashRouter as Router, Route, Routes} from "react-router-dom";
-//
-// class App extends Component {
-//     // constructor(props) {
-//     //     super(props);
-//     //
-//     //     history.listen((location) => {
-//     //         props.dispatch(clearMessage()); // clear message when changing location
-//     //     });
-//     // }
-//
-//     render() {
-//         return (
-//             // <BrowserRouter history={history}>
-//             <Router>
-//                 <Routes>
-//                     <Route>
-//                         <Route path="/" element={<Resources />} />
-//                         <Route path="/about" element={<About />} />
-//                     </Route>
-//                 </Routes>
-//                 {/*<AppRoutes />*/}
-//             </Router>
-//
-//             // </BrowserRouter>
-//         )
-//     }
-// }
-//
-// function About() {
-//     return(
-//         <p>About</p>
-//     )
-// }
-// function Resources() {
-//     return(
-//         <p>Resources</p>
-//     )
-// }
-// export default App;
+import AppRoutes from './components/AppRoutes';
+import {Component} from "react";
+import {HashRouter as Router} from "react-router-dom";
 
-
-
-function App() {
-    return (
-        <div className="App">
-            <Resources />
-
-        </div>
-    );
+class App extends Component {
+    render() {
+        return (
+            <Router>
+                <AppRoutes />
+            </Router>
+        )
+    }
 }
-
-function Resources() {
-    // console.error(process.env.REACT_APP_TEST)
-    return(
-        <p>dddddddddd {process.env.REACT_APP_API_URL}</p>
-        // <p>{process.env.REACT_APP_API_URL}</p>
-    )
-}
-
-
 export default App;
