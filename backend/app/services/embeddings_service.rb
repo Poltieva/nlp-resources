2c0d32d1f99f18d981398f642800865b12ca9d56
Anna Poltieva <poltyeva.anna@gmail.com>
2023-04-26 16:14:36 +0200

    Add embeddings for descriptions

---
+++ backend/app/services/embeddings_service.rb
@@ -0,0 +1,15 @@
+class EmbeddingsService
+  def dump(resource)
+    File.write("#{Rails.root}/tmp/#{resource.id}.description.txt", resource.description)
+    # File.write("#{Rails.root}/tmp/#{resource.id}.keywords.txt", resource.keywords)
+    `python3 ../embeddings/embed.py #{resource.id}`
+    File.delete("#{Rails.root}/tmp/#{resource.id}.description.txt")
+    puts "Dumped embeddings for resource ##{resource.id}"
+    # File.delete("#{Rails.root}/tmp/#{resource.id}.keywords.txt")
+  end
+
+  def fetch_candidates_for(query)
+    output = `python3 ../embeddings/encode_query.py #{query}`
+    # eval output
+  end
+end
--- embeddings/encode_query.py
+++ embeddings/encode_query.py
@@ -1,14 +1,52 @@
 from sentence_transformers import SentenceTransformer
 from sys import argv, exit
+import pickle
+import tensorflow as tf
+import numpy as np
 
-def encode_query(query):
+def encode_query(model, query, tags = []):
     try:
-        embedding = SentenceTransformer('paraphrase-multilingual-MiniLM-L12-v2').encode(query)
+        query_embedding = model.encode(query)     
+        # tags_embedding = np.mean(model.encode(tags), axis = 0)
     except:
         print("Error encoding query")
         exit(1)
 
-    print(embedding.tolist())
+    # return np.concatenate([query_embedding, tags_embedding], axis=0)
+    return query_embedding
+
+def load_embeddings():
+    in_path = './db/embeddings.pkl'
+
+    books_data = []
+    with open(in_path, 'rb') as f:
+        while True:
+            try:
+                book_embeddings = pickle.load(f)
+                books_data.append(book_embeddings)
+            except EOFError:
+                break
+    return books_data
+
+def cosine_similarities(a, b):
+    return tf.matmul(a, b, transpose_b = True)
+
+def main(model, query, tags = []):
+    DB_data = load_embeddings()
+    DB_embeddings = tf.constant([list(d.values())[0] for d in DB_data])
+    DB_embeddings = tf.nn.l2_normalize(DB_embeddings, axis=1)     
+    query_embedding = encode_query(model, query, tags)
+
+    query_embedding = tf.nn.l2_normalize(tf.expand_dims(tf.constant(query_embedding),0), axis=1)
+    scores = cosine_similarities(query_embedding, DB_embeddings)
+    top_k_indices = tf.math.top_k(scores, 10)[1].numpy().squeeze().tolist()
+
+    # create list of tuples containing ID and embedding
+    candidates = [list(DB_data[i].keys())[0] for i in top_k_indices]
+
+    return candidates
 
 if __name__ == '__main__':
-    encode_query(argv[1])
+    model = SentenceTransformer('paraphrase-multilingual-MiniLM-L12-v2')
+    query = argv[1]
+    print(main(model, query))
